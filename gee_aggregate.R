0f9c6345debbf8fb47c821d842f6078903877ed6
Keith Jennings <keithjennings@Keiths-MacBook-Pro.local>
2020-09-02 11:53:29 -0600

    script for aggregating processed Google Earth Engine data

---
+++ gee_aggregate.R
@@ -0,0 +1,44 @@
+# Script for aggregating surface reflectance data processed
+# in Google Earth Engine
+
+# Keith Jennings
+# kjennings@lynkertech.com
+# 2020-09-01
+
+# Load packages
+library(tidyverse)
+
+# Add data directory string
+data.dir <- "../data/gee/"
+
+# List files in Google Earth Engine folder
+files <- list.files(data.dir)
+
+# Import files into list
+data.l <- lapply(paste0(data.dir, files), read.csv, stringsAsFactors = F)
+
+# Bind into a single data frame
+df <- plyr::ldply(data.l, bind_rows)
+
+# Add prefix to beginning of sentinel 2 strings to match landsat patterns
+# Add sensor and date columns
+df <- df %>% 
+  mutate(system.index = case_when(str_sub(system.index, 1, 2) == "20" ~ 
+                                     paste0("SE02_123456_", system.index),
+                                   TRUE ~ system.index),
+         sensor = str_sub(system.index, 1, 4),
+         date = str_sub(system.index, 13, 20) %>% as.Date(format = "%Y%m%d"))
+
+# Put QA values into same column
+# Identify whether data are from a sampling point or waterbody average
+df <- df %>% 
+  mutate(qa_val = case_when(!is.na(pixel_qa) ~ pixel_qa,
+                            !is.na(QA60) ~ QA60),
+         type = case_when(!is.na(id2) ~ "pt",
+                          TRUE ~ "wb"))
+
+# Export as RDS file
+df %>% 
+  select(sensor, date, type, id2, bcpos_id, osmp_id, 
+         BLU:hrwi, lswi:nir_red_br, qa_val) %>% 
+  saveRDS(paste0(data.dir, "sensors_all_aggregated.RDS"))
