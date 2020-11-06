f8c42ee55079e551ed4b83f0693518e75ca6b7fd
Keith Jennings <keithjennings@Keiths-MacBook-Pro.local>
2020-11-06 13:43:42 -0700

    toa vs. sr updates

--- sentinel2_sr_vs_toa_comparison.R
+++ sentinel2_sr_vs_toa_comparison.R
@@ -17,5 +17,35 @@ df <- left_join(
            stringsAsFactors = F),
   read.csv(paste0(data.dir, "sentinel2_toa_pts.csv"),
            stringsAsFactors = F),
-  by = "system.index"
+  by = c("system.index", "id2")
 )
+
+# Extract date and sensor info from system.index
+df <- df %>% 
+  mutate(system.index = case_when(str_sub(system.index, 1, 2) == "20" ~ 
+                                    paste0("SE02_123456_", system.index),
+                                  TRUE ~ system.index),
+         sensor = str_sub(system.index, 1, 4),
+         date = str_sub(system.index, 13, 20) %>% as.Date(format = "%Y%m%d"))
+
+# Plot comparison
+
+# Plot nir_red_br
+filter(df, id2 == 254) %>% 
+  ggplot()+
+  geom_line(aes(date, nir_red_br.x)) + 
+  geom_line( aes(date, nir_red_br.y), color = "red")
+
+# Add FAI data 
+df <- df %>% 
+  mutate(fai2.x = NIR.x/10000 - (RED.x/10000 + (SWIR1.x/10000 - RED.x/10000) * ((865 - 655) / (1610 - 655))),
+         fai2.y = NIR.y/10000 - (RED.y/10000 + (SWIR1.y/10000 - RED.y/10000) * ((865 - 655) / (1610 - 655))))
+
+# Plot the FAI
+filter(df, id2 == 254) %>% 
+  ggplot()+
+  geom_line(aes(date, fai2.x)) + 
+  geom_line( aes(date, fai2.y), color = "red")
+
+
+
