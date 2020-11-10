6acc02ca2cb91460bba8689c3c7149556a2517fa
Keith Jennings <keithjennings@Keiths-MacBook-Pro.local>
2020-11-10 09:08:29 -0700

    Updates to analysis scripts

--- gee_plot.R
+++ gee_plot.R
@@ -7,12 +7,43 @@
 # Load packages
 library(tidyverse)
 library(cowplot); theme_set(theme_cowplot())
+library(lubridate)
 
 # Add data directory string
 data.dir <- "../data/gee/"
 
 # Import processed data
-gee <- readRDS(paste0(data.dir, "sensors_all_aggregated.RDS"))
+gee <- readRDS(paste0(data.dir, "processed/sensors_all_aggregated.RDS"))
+
+# Import pt metadata
+pts_meta <- read.csv("../data/geospatial/metadata/osmp_sampling_pts_meta.csv")
+
+# Add month and year columns
+gee <- gee %>% 
+  mutate(month = month(date),
+         year = year(date))
+
+# Join the pts metadata
+gee <- left_join(gee, 
+                 pts_meta,
+                 by = "id2")
+
+# Compute % of obs over nir:red thresh per year, per sensor
+# note: could also do this per point
+# need to add edge info via metadata 
+nir_red_thresh <- 1.5
+pts_nir_red_over <- gee %>% 
+  filter(month %in% 4:10 & !is.na(id2)) %>% 
+  group_by(sensor, year, edge) %>% 
+  summarise(n_over = sum(nir_red_br >= nir_red_thresh),
+            n_obs = n()) %>% 
+  mutate(pct_over = n_over / n_obs * 100)
+
+
+# plot
+ggplot(pts_nir_red_over, aes(year, pct_over, color = sensor)) + 
+  geom_line() + 
+  facet_wrap(~edge)
 
 # Compute new FAI
 # 'NIR - (RED + (SWIR1 - RED) * ((865 - 655) / (1610 - 655)))'
--- sentinel2_sr_vs_toa_comparison.R
+++ sentinel2_sr_vs_toa_comparison.R
@@ -31,7 +31,7 @@ df <- df %>%
 # Plot comparison
 
 # Plot nir_red_br
-filter(df, id2 == 254) %>% 
+filter(df, id2 == 2) %>% 
   ggplot()+
   geom_line(aes(date, nir_red_br.x)) + 
   geom_line( aes(date, nir_red_br.y), color = "red")
@@ -42,10 +42,28 @@ df <- df %>%
          fai2.y = NIR.y/10000 - (RED.y/10000 + (SWIR1.y/10000 - RED.y/10000) * ((865 - 655) / (1610 - 655))))
 
 # Plot the FAI
-filter(df, id2 == 254) %>% 
+filter(df, id2 == 3) %>% 
   ggplot()+
   geom_line(aes(date, fai2.x)) + 
   geom_line( aes(date, fai2.y), color = "red")
+
+
+# Add NDWI from NIR and SWIR1
+df <- df %>% 
+  mutate(ndwi.x = (NIR.x - SWIR1.x) / (NIR.x + SWIR1.x) ,
+         ndwi.y = (NIR.y - SWIR1.y) / (NIR.y + SWIR1.y))
+
+# Plot the NDWI
+filter(df, id2 == 1) %>% 
+  ggplot()+
+  geom_line(aes(date, ndwi.x)) + 
+  geom_line( aes(date, ndwi.y), color = "red")
+
+
+# Determine which points meet Oyama cyonabac thresholds
+test <- filter(df, ndwi.y > 0.6 & fai2.y > 0.05)
+unique(test$id2)
+test_summary <- test %>% group_by(id2) %>% summarise(n = n())
 
 
 
