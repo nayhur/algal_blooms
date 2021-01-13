eb3b200ce18f716f3133bff7377beef79fa23035
Keith Jennings <keithjennings@keiths-mbp.lan>
2021-01-13 08:56:37 -0700

    Updated plots and analysis for report

--- gee_plot.R
+++ gee_plot.R
@@ -11,12 +11,13 @@ library(lubridate)
 
 # Add data directory string
 data.dir <- "../data/gee/"
+plot.dir <- "../plots/"
 
 # Import processed time series data
 gee <- readRDS(paste0(data.dir, "processed/sensors_all_aggregated.RDS"))
 
 # Import pt metadata
-pts_meta <- read.csv("../data/geospatial/metadata/osmp_sampling_pts_meta.csv")
+pts_meta <- read.csv("../data/geospatial/metadata/osmp_sampling_pts_meta_v2.csv")
 
 # Join the pts metadata
 gee <- left_join(gee, 
@@ -76,17 +77,21 @@ pts_fai_ndwi_over <- gee %>%
 
 # plot
 # plot
-filter(pts_fai_ndwi_over, sensor %in% c("LT05", "LE07") & year >= 1990) %>% 
+pts_fai_ndwi_over_plot <-
+  filter(pts_fai_ndwi_over, sensor %in% c("LT05", "LE07") & year >= 1990) %>% 
   ggplot( aes(year, pct_over, color = sensor)) + 
   geom_line(lwd = 1) + 
   facet_wrap(~edge2) +
   labs(x = "Year", 
-       y = "Observations with NIR:Red > 1 (%)") +
+       y = "Obs. Over FAI & NDWI Thresholds (%)") +
   scale_color_manual(name = "Sensor", 
                      breaks = c("LT05", "LE07"),
                      labels = c("Landsat 5", "Landsat 7"),
                      values = c("brown", "cyan4")) +
   theme(axis.text.x = element_text(angle = -30, hjust =0))
+save_plot(plot = pts_fai_ndwi_over_plot,
+          filename = paste0(plot.dir, "pts_fai_ndwi_over_by_year.png"),
+          base_height = 5, base_width = 7)
 
 # Analysis showing NIR:Red evolution at Wonderland Lake for LT05 versus LE07
 #make 10 day movign average filter
@@ -108,3 +113,66 @@ ggplot(wonderland, aes(doy, nir_red_br_av_ma10, color = sensor))+
   theme(axis.text.x = element_text(angle = -30, hjust =0)) +
   scale_x_continuous(breaks = c(91, 152, 213, 274),
                      labels = c("Apr. 1", "Jun. 1", "Aug. 1", "Oct. 1"))
+
+
+
+##
+# Summarize the pct of occurrences per waterbody for L5 and l7 for near edge and open
+wbs_nir_over <- filter(gee, !is.na(edge2) & month %in% 6:10) %>% 
+  group_by(waterbody, edge2) %>% 
+  summarise(n_over = sum(nir_red_br >= nir_red_thresh),
+            n_obs = n()) %>% 
+  mutate(pct_over = n_over / n_obs * 100)
+
+# Plot
+wbs_nir_over_plot <-
+  ggplot(wbs_nir_over, aes(waterbody, pct_over)) + 
+  geom_bar(stat = "identity", color = "black", fill = "darkgreen") + 
+  theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = 0.5)) + 
+  facet_wrap(~edge2, ncol = 1) + 
+  labs(x = "Waterbody", 
+       y = "Observations Over NIR:Red Threshold (%)")
+save_plot(plot = wbs_nir_over_plot,
+          filename = paste0(plot.dir, "nir_red_over_by_waterbody.png"),
+          base_height = 8, base_width = 12)
+##
+# Summarize the pct of occurrences per waterbody for L5 and l7 for near edge and open
+wbs_fai_over <- filter(gee, !is.na(edge2) & month %in% 6:10) %>% 
+  group_by(waterbody, edge2) %>% 
+  summarise(n_over = sum(cyano),
+            n_obs = n()) %>% 
+  mutate(pct_over = n_over / n_obs * 100)
+
+# Plot
+wbs_fai_over_plot <-
+  ggplot(wbs_fai_over, aes(waterbody, pct_over)) + 
+  geom_bar(stat = "identity", color = "black", fill = "darkblue") + 
+  theme(axis.text.x = element_text(angle = 270, hjust = 0, vjust = 0.5)) + 
+  facet_wrap(~edge2, ncol = 1) + 
+  labs(x = "Waterbody", 
+       y = "Obs. Over FAI & NDWI Thresholds (%)")
+save_plot(plot = wbs_fai_over_plot,
+          filename = paste0(plot.dir, "fai_ndwi_over_by_waterbody.png"),
+          base_height = 8, base_width = 12)
+
+
+################################################################################
+################################################################################
+# Some data analyses:
+
+# View top and bottom NIR open water exceedances
+filter(wbs_nir_over, edge2 == "Open Water") %>% arrange(pct_over) %>% head(n = 15)
+filter(wbs_nir_over, edge2 == "Open Water") %>% arrange(-pct_over) %>% head(n = 10)
+
+# View top and bottom FAI & NDWI open water exceedances
+filter(wbs_fai_over, edge2 == "Open Water") %>% arrange(pct_over) %>% head(n = 15)
+filter(wbs_fai_over, edge2 == "Open Water") %>% arrange(-pct_over) %>% head(n = 15)
+
+# Compare Landsat 7 2001-2010 to 2011-2020 for each pixel type
+filter(pts_nir_red_over, year %in% 1999:2009 & sensor == "LE07") %>% 
+  group_by(edge2) %>% 
+  summarise(pct_over_av = mean(pct_over))
+filter(pts_nir_red_over, year %in% 2010:2020 & sensor == "LE07") %>% 
+  group_by(edge2) %>% 
+  summarise(pct_over_av = mean(pct_over))
+
